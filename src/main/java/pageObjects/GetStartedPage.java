49d2c093bf8e21ad5953390d0032251d620e93a2
showri <showrijb@gmail.com>
2025-12-08 22:42:29 -0500

    added few java pages

---
+++ src/main/java/pageObjects/GetStartedPage.java
@@ -0,0 +1,14 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class GetStartedPage{
+
+    WebDriver driver;
+
+    public GetStartedPage(WebDriver driver) {
+        this.driver = driver;
+       
+    }
+
+}
---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,7 @@
+package hooks;
+
+
+public class Hooks {
+
+    
+}
